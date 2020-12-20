44b810edccb0a1380624b3079474991859716da3
Louis Faucon <lpfaucon@gmail.com>
2020-12-20 15:28:03 +0100

    Replaces class by className

--- src/components/GamePicker.js
+++ src/components/GamePicker.js
@@ -39,7 +39,7 @@ const GamePicker = ({ setGameId, logger }) => {
       <h1>ELEUSIS 2</h1>
       <h2>Welcome!</h2>
       <span>Join a game:</span>
-      <div class="inputContainer">
+      <div className="inputContainer">
         <input
           type="text"
           placeholder="Game ID"
